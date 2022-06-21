d66e6938d65896dcae6b75066cfb3926cbfcb1da
MaiPhuong1505 <maiphuongpham1505@gmail.com>
2022-06-22 01:02:12 +0700

    final

--- customer/models.py
+++ customer/models.py
@@ -9,7 +9,7 @@ class MenuItem(models.Model):
     description = models.TextField()
     image = models.ImageField(upload_to='menu_images/')
     price = models.DecimalField(max_digits=5, decimal_places=0)
-    category = models.ManyToManyField('Category',related_name='item') 
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, blank=False, null=True) 
 
     def __str__(self):
         return self.name
@@ -26,14 +26,14 @@ class OrderModel(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     price = models.DecimalField(max_digits=7, decimal_places=0)
     items = models.ManyToManyField('MenuItem', related_name='order', blank=True)
-    name = models.CharField(max_length=50, blank=True)
+    name = models.CharField(max_length=50, blank=False, null=True)
     email = models.CharField(max_length=50, blank=True)
-    street = models.CharField(max_length=50, blank=True)
-    ward = models.CharField(max_length=50, blank=True)
-    district = models.CharField(max_length=50, blank=True)
-    city = models.CharField(max_length=50, blank=True)
+    street = models.CharField(max_length=50, blank=False, null=True)
+    ward = models.CharField(max_length=50, blank=False, null=True)
+    district = models.CharField(max_length=50, blank=False, null=True)
+    city = models.CharField(max_length=50, blank=False, null=True)
     is_paid = models.BooleanField(default=False)
     is_shipped = models.BooleanField(default=False)
 
     def __str__(self):
-        return f'Order: {self.created_on.strftime("%b %d %I: %M %p")}'
+        return f'Order: {self.created_on.strftime("%m/%d/%y %H:%M:%S")}'
